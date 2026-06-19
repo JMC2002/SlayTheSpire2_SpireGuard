d8079c38e213ca18c78875bf31f2d77a6114c8bf
MC J <jmc8@qq.com>
2026-06-19 14:50:38 +0800

    适配创意工坊

--- Core/VersionInfo.cs
+++ Core/VersionInfo.cs
@@ -3,6 +3,6 @@ namespace SpireGuard.Core;
 public static class VersionInfo
 {
     public const string Name = "SpireGuard";
-    public const string Version = "1.1.0";
+    public const string Version = "1.2.0";
     public static string Tag => $"[{Name} v{Version}]";
 }
